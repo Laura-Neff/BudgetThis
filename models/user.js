5e323f5e06752ea0fed03086d9548698ae1ec888
hngan <hngan@cs.stonybrook.edu>
2019-07-16 14:09:12 -0400

    Input form

--- models/user.js
+++ models/user.js
@@ -4,8 +4,6 @@ module.exports = function(sequelize, DataTypes) {
         budget: DataTypes.REAL
     });
     User.associate = function(models) {
-        // Associating Author with Posts
-        // When an Author is deleted, also delete any associated Posts
         User.hasMany(models.Transactions, {
             onDelete: "cascade"
         });
