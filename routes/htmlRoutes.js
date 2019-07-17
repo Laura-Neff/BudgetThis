a607cb02e837ae42bd0bdb5332f33e12efa7daec
Laura-Neff <ljneff@gmail.com>
2019-07-17 13:13:52 -0400

    mid-way changing html routes to handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,42 +6,49 @@ module.exports = function(app) {
   // Load index page
 
   // home route
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
+
+  // // transaction.html route
+  // app.get("/history", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/transaction.html"));
+  // });
+
+  // // input transaction route
+  // app.get("/new", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/input.html"));
+  // });
+
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
+
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
   });
 
-  // transaction.html route
+  // Load example page and pass in an example by id
   app.get("/history", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/transaction.html"));
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
   });
 
-  // input transaction route
   app.get("/new", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/input.html"));
   });
 
+  // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-
-  // // Load example page and pass in an example by id
-  // app.get("/example/:id", function(req, res) {
-  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-
-  // Render 404 page for any unmatched routes
 };
 
