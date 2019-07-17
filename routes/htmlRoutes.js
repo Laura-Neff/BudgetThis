9b42260c469b22bde9aac9e637e038afb9d900c7
Laura-Neff <ljneff@gmail.com>
2019-07-17 13:21:43 -0400

    Fixed html routes to render for handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,7 @@
 var db = require("../models");
-var path = require("path");
+// var path = require("path");
 
 module.exports = function(app) {
-<<<<<<< HEAD
-    // Load index page
-    app.get("/", function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.render("index", {
-                msg: "Welcome!",
-                examples: dbExamples
-            });
-        });
-    });
-
-    // Load example page and pass in an example by id
-    app.get("/example/:id", function(req, res) {
-        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.render("example", {
-                example: dbExample
-            });
-        });
-    });
-
-    // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-        res.render("404");
-    });
-};
-=======
-
   // Load index page
 
   // home route
@@ -52,8 +25,8 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
+      res.render("home", {
+        msg: "Welcome home!",
         examples: dbExamples
       });
     });
@@ -62,14 +35,20 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/history", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
+      res.render("transaction", {
+        msg: "Transaction history!",
         example: dbExample
       });
     });
   });
 
   app.get("/new", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/input.html"));
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("input", {
+        msg: "Input a transaction here",
+        example: dbExample
+      });
+    });
   });
 
   // Render 404 page for any unmatched routes
@@ -77,5 +56,3 @@ module.exports = function(app) {
     res.render("404");
   });
 };
-
->>>>>>> e72b120915d9f6c1df5d83ed5305d38a4ebaae23
