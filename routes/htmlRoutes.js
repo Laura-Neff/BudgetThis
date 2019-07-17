aeb057bc1a6a79a900d3ef26c981bcbb384a8d45
hngan <hngan@cs.stonybrook.edu>
2019-07-17 13:48:43 -0400

    Fixed naming for user

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,46 +1,58 @@
 var db = require("../models");
-var path = require("path");
+// var path = require("path");
 
 module.exports = function(app) {
-
     // Load index page
 
     // home route
+    // app.get("/", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/home.html"));
+    // });
+
+    // // transaction.html route
+    // app.get("/history", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/transaction.html"));
+    // });
+
+    // // input transaction route
+    // app.get("/new", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/input.html"));
+    // });
+
+    // app.get("*", function(req, res) {
+    //   res.render("404");
+    // });
+
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        db.Example.findAll({}).then(function(dbExamples) {
+            res.render("home", {
+                msg: "Welcome home!",
+                examples: dbExamples
+            });
+        });
     });
 
-    // transaction.html route
+    // Load example page and pass in an example by id
     app.get("/history", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/transaction.html"));
+        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.render("transaction", {
+                msg: "Transaction history!",
+                example: dbExample
+            });
+        });
     });
 
-    // input transaction route
     app.get("/new", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/input.html"));
+        db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+            res.render("input", {
+                msg: "Input a transaction here",
+                example: dbExample
+            });
+        });
     });
 
+    // Render 404 page for any unmatched routes
     app.get("*", function(req, res) {
         res.render("404");
     });
-
-    // app.get("/", function(req, res) {
-    //   db.Example.findAll({}).then(function(dbExamples) {
-    //     res.render("index", {
-    //       msg: "Welcome!",
-    //       examples: dbExamples
-    //     });
-    //   });
-    // });
-
-    // // Load example page and pass in an example by id
-    // app.get("/example/:id", function(req, res) {
-    //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-    //     res.render("example", {
-    //       example: dbExample
-    //     });
-    //   });
-    // });
-
-    // Render 404 page for any unmatched routes
 };
