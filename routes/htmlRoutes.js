2bd91d482fdaf4f6b000e5327f49bfd35fa2306e
Arsteinberg <arsteinberg93@gmail.com>
2019-07-18 13:50:04 -0400

    fixed error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -47,33 +47,25 @@ module.exports = function(app) {
         });
     });
 
-<<<<<<< HEAD
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+    // Render 404 page for any unmatched routes
+    app.get("*", function(req, res) {
+        res.render("404");
+    });
 };
 
 var queryUrl = "https://financialmodelingprep.com/api/v3/stock/gainers";
 
 axios
-  .get(queryUrl)
-  .then(function(response) {
-    // console.log(response);
-    for (i = 0; i < response.data.mostGainerStock.length; i++) {
-      // response.data[i];
-      console.log(response.data.mostGainerStock[i]);
-    }
-  })
-  .catch(function(error) {
-    if (error.response) {
-      console.log(error.response);
-    }
-  });
-=======
-    // // Render 404 page for any unmatched routes
-    // app.get("*", function(req, res) {
-    //     res.render("404");
-    // });
-};
->>>>>>> ae475233b5e80cdfda8ce8b7b55c9800e871e1af
+    .get(queryUrl)
+    .then(function(response) {
+        // console.log(response);
+        for (i = 0; i < response.data.mostGainerStock.length; i++) {
+            // response.data[i];
+            console.log(response.data.mostGainerStock[i]);
+        }
+    })
+    .catch(function(error) {
+        if (error.response) {
+            console.log(error.response);
+        }
+    });
