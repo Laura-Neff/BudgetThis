49a46417d4988549bafe075fa3a31116550458fc
Arsteinberg <arsteinberg93@gmail.com>
2019-07-18 13:33:58 -0400

    Investment API added

--- models/user.js
+++ models/user.js
@@ -1,12 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-        name: DataTypes.STRING,
-        budget: DataTypes.REAL
+  var User = sequelize.define("User", {
+    name: DataTypes.STRING,
+    budget: DataTypes.REAL
+  });
+  User.associate = function(models) {
+    User.hasMany(models.Transaction, {
+      onDelete: "cascade"
     });
-    User.associate = function(models) {
-        User.hasMany(models.Transaction, {
-            onDelete: "cascade"
-        });
-    };
-    return User;
+  };
+  return User;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+var axios = require("axios");
+
 // var path = require("path");
 
 module.exports = function(app) {
@@ -50,3 +52,20 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+
+var queryUrl = "https://financialmodelingprep.com/api/v3/stock/gainers";
+
+axios
+  .get(queryUrl)
+  .then(function(response) {
+    // console.log(response);
+    for (i = 0; i < response.data.mostGainerStock.length; i++) {
+      // response.data[i];
+      console.log(response.data.mostGainerStock[i]);
+    }
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log(error.response);
+    }
+  });
